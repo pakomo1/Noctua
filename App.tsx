9b40ed2b3c6686bdbb1cec2ad45e165b7ac12919
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-11-25 17:13:34 +0200

    Making the screen in landscape mode and adding style

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Camera, runAtTargetFps, useCameraDevice, useCameraFormat, useCameraPermission, useCodeScanner, useFrameProcessor } from 'react-native-vision-camera';
-import { useSharedValue } from 'react-native-worklets-core';
 import { xyz } from './XYZFrame';
-import VoiceInputScreen from './components/Voice';
+import Voice from '@react-native-voice/voice';
 
 export default function App() {
+  const [isListening, setIsListening] = useState(false);
+  const [recognizedText, setRecognizedText] = useState('');
+
+  useEffect(() => {
+    Voice.onSpeechResults = (results: any) => {
+      setRecognizedText(results.value);
+    };
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startListening = async () => {
+    try {
+      await Voice.start('en-US');
+      console.log('we are here now');
+      setIsListening(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const stopListening = async () => {
+    try {
+      await Voice.stop();
+      setIsListening(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const { hasPermission, requestPermission } = useCameraPermission()
   const device = useCameraDevice('back')!
   const format = useCameraFormat(device, [
@@ -14,19 +44,15 @@ export default function App() {
     { fps: 24 },
     { photoHdr: false },
     { videoHdr: false },
-
   ])
-  const debugTyped = useSharedValue(false)
-  const cFrame = useSharedValue(new Uint8Array)
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-    //cFrame.value = frame.toArrayBuffer()
     runAtTargetFps(18, () => {
       'worklet'
-      const result = xyz(frame)
+      //const result = xyz(frame)
 
-      console.log(result)
+      //console.log(result)
     })
   }, [])
 
@@ -35,11 +61,12 @@ export default function App() {
     return <View style={{ backgroundColor: 'grey' }}></View>
   }
 
+  const someValues = "0.124215531";
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Camera
-        style={{ flex: 1, width: '100%', height: '100%' }}
+        style={{ flex: 1, width: '100%', height: '100%', position: 'absolute' }}
         device={device}
         isActive={true}
         frameProcessor={frameProcessor}
@@ -47,7 +74,14 @@ export default function App() {
         pixelFormat='yuv'
         enableZoomGesture={true}
       />
-      <VoiceInputScreen/>
+      <TouchableOpacity style={styles.startListeningButton} onPress={isListening ? stopListening : startListening}>
+        <View style={styles.mainComponentsWrapper}>
+          <Text style={{ fontSize: 30, fontWeight: '800' }}>Turning Weight:</Text>
+          <Text style={{ fontSize: 30, marginBottom: 20, fontWeight:'500' }}>{parseFloat(someValues).toPrecision(2)}</Text>
+          <Text style={styles.voiceCommandsText}>{isListening ? 'Listening...' : 'Not listening'}</Text>
+          <Text style={styles.voiceCommandsText}>Recognized Text: {recognizedText}</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -59,4 +93,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  startListeningButton: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center'
+  },
+  mainComponentsWrapper: {
+    alignItems: 'center',
+    borderRadius: 30,
+    padding: 20,
+    backgroundColor: 'grey',
+    width: '30%',
+    alignSelf: 'center',
+  },
+  voiceCommandsText: {
+    fontSize: 20
+  }
 });
